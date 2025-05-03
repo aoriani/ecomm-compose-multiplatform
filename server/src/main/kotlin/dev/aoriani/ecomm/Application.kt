a0ae46fa23229c19d71d2b7ea54f6dccf19859f6
André Oriani <aoriani@gmail.com>
2025-05-03 07:19:39 -0700

    Tracing error 2

--- server/src/main/kotlin/dev/aoriani/ecomm/Application.kt
+++ server/src/main/kotlin/dev/aoriani/ecomm/Application.kt
@@ -6,11 +6,14 @@ import dev.aoriani.ecomm.graphql.queries.ProductQuery
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.util.logging.KtorSimpleLogger
+
+internal val LOGGER = KtorSimpleLogger("com.example.RequestTracePlugin")
 
 fun main(args: Array<String>) {
     println("🚀 Dumping environment variables before starting Ktor:")
     System.getenv().forEach { (key, value) ->
-        println("$key = ${if (value.isBlank()) "<empty>" else value}")
+        LOGGER.info("$key = ${if (value.isBlank()) "<empty>" else value}")
     }
     EngineMain.main(args)
 }
