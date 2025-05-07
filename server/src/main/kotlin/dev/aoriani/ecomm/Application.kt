2598cb304bed084c986e6f758435f53d7f770584
André Oriani <aoriani@gmail.com>
2025-05-06 20:55:34 -0700

    attempt to fix cors

--- server/src/main/kotlin/dev/aoriani/ecomm/Application.kt
+++ server/src/main/kotlin/dev/aoriani/ecomm/Application.kt
@@ -3,6 +3,8 @@ package dev.aoriani.ecomm
 import com.expediagroup.graphql.server.ktor.GraphQL
 import com.expediagroup.graphql.server.ktor.defaultGraphQLStatusPages
 import dev.aoriani.ecomm.graphql.queries.ProductQuery
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.plugins.cors.routing.CORS
@@ -17,10 +19,23 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     install(CORS) {
-        anyHost()
-        allowHeaders { true }
-        anyMethod()
+        allowHost("localhost:8080", schemes = listOf("http"))
+        allowHost("aoriani.dev", schemes = listOf("https"))
+
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Options)
+
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("X-Requested-With") // se necessário
+
+        allowCredentials = false // ou true, se estiver enviando cookies/autenticação
     }
+
     install(GraphQL) {
         schema {
             packages = listOf("dev.aoriani.ecomm.graphql.models")
