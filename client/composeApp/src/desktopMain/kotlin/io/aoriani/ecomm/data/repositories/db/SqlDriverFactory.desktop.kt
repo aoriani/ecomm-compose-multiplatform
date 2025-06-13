d2d78c3311ed80bee560f8aaace63c19127ec95b
André Oriani <aoriani@gmail.com>
2025-06-12 23:44:42 -0700

    Implement SQLite driver for Desktop
    - Added JdbcSqliteDriver for desktop platform.
    - Configured the driver to use "jdbc:sqlite:test.db".
    - Used `ProductDatabase.Schema.synchronous()` for schema creation.

--- client/composeApp/src/desktopMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.desktop.kt
+++ client/composeApp/src/desktopMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.desktop.kt
@@ -1,9 +1,16 @@
 package io.aoriani.ecomm.data.repositories.db
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.util.Properties
 
 actual class SqlDriverFactory {
     actual fun createDriver(): SqlDriver {
-        TODO("Not yet implemented")
+        return JdbcSqliteDriver(
+            url = "jdbc:sqlite:test.db",
+            properties = Properties(),
+            schema = ProductDatabase.Schema.synchronous()
+        )
     }
 }
