f0c3e99e440d5c568df24efc6658daf452066143
André Oriani <aoriani@gmail.com>
2025-06-14 11:02:39 -0700

    feat: Implement SqlDriverFactory for wasmJs
    - Added `WebWorkerDriver` for wasmJs platform.
    - The driver uses a Web Worker with `sqljs.worker.js`.
    - Initializes the `ProductDatabase` schema after creating the driver.

--- client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
+++ client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
@@ -4,13 +4,12 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import org.w3c.dom.Worker
 
+private val sqlJsWorkerUrl: String = js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
+
 actual class SqlDriverFactory {
     actual fun createDriver(): SqlDriver {
-        @Suppress("UNREACHABLE_CODE")
         return WebWorkerDriver(
-            Worker(
-                js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
-            )
+            Worker(sqlJsWorkerUrl)
         ).also {
             ProductDatabase.Schema.create(it)
         }
