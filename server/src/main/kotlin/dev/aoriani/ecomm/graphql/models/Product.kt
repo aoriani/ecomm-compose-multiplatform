59678e7e20268658cf431afe347243f361a7603f
André Oriani <aoriani@gmail.com>
2025-06-20 00:06:02 -0700

    feat: Add GraphQL descriptions for Product model and queries
    - Annotated `Product` model properties with `@GraphQLDescription` for better API documentation.
    - Enhanced `ProductQuery` class with descriptive annotations for methods and parameters.

--- server/src/main/kotlin/dev/aoriani/ecomm/graphql/models/Product.kt
+++ server/src/main/kotlin/dev/aoriani/ecomm/graphql/models/Product.kt
@@ -1,12 +1,30 @@
 package dev.aoriani.ecomm.graphql.models
 
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+
+@GraphQLDescription("Represents a product available in the e-commerce catalog")
 data class Product(
+    @GraphQLDescription("Unique identifier of the product")
     val id: String,
+
+    @GraphQLDescription("Name of the product")
     val name: String,
+
+    @GraphQLDescription("Price of the product in USD")
     val price: Double,
+
+    @GraphQLDescription("Detailed description of the product")
     val description: String,
+
+    @GraphQLDescription("List of image URLs associated with the product")
     val images: List<String>,
+
+    @GraphQLDescription("Material composition of the product")
     val material: String,
+
+    @GraphQLDescription("Indicates whether the product is currently in stock")
     val inStock: Boolean,
-    val countryOfOrigin: String,
+
+    @GraphQLDescription("Country where the product was manufactured")
+    val countryOfOrigin: String
 )
--- server/src/main/kotlin/dev/aoriani/ecomm/graphql/queries/ProductQuery.kt
+++ server/src/main/kotlin/dev/aoriani/ecomm/graphql/queries/ProductQuery.kt
@@ -1,10 +1,19 @@
 package dev.aoriani.ecomm.graphql.queries
 
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.server.operations.Query
-import dev.aoriani.ecomm.graphql.repository.ProductRepository
 import dev.aoriani.ecomm.graphql.models.Product
+import dev.aoriani.ecomm.graphql.repository.ProductRepository
 
-class ProductQuery: Query {
+@GraphQLDescription("Root entry point for product-related queries")
+class ProductQuery : Query {
+
+    @GraphQLDescription("Retrieve all products available in the catalog")
     fun products(): List<Product> = ProductRepository.getAll()
-    fun product(id: String): Product? = ProductRepository.getById(id)
+
+    @GraphQLDescription("Fetch a single product by its unique identifier")
+    fun product(
+        @GraphQLDescription("The unique ID of the product to retrieve")
+        id: String
+    ): Product? = ProductRepository.getById(id)
 }
