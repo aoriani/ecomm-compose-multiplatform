f9fbdc9782b4687ab83cc65a8e563757cf5f1ca3
André Oriani <aoriani@gmail.com>
2025-06-29 00:20:39 -0700

    Implement DollarAmount KMP class
    - Defined `expect class DollarAmount` in commonMain with a constructor taking a String, and `plus` and `times` operators.
    - Implemented `actual class DollarAmount` for Android and Desktop using `java.math.BigDecimal`.
    - Implemented `actual class DollarAmount` for iOS using `platform.Foundation.NSDecimalNumber`.
    - Implemented `actual class DollarAmount` for WasmJs using a `BigJs` external class (imported as `Big`).

--- client/composeApp/src/desktopMain/kotlin/io/aoriani/ecomm/data/model/DollarAmount.desktop.kt
+++ client/composeApp/src/desktopMain/kotlin/io/aoriani/ecomm/data/model/DollarAmount.desktop.kt
@@ -1,3 +1,16 @@
 package io.aoriani.ecomm.data.model
 
-actual class DollarAmount
+import java.math.BigDecimal
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class DollarAmount private constructor(private val rawValue: BigDecimal) {
+    actual constructor(value: String) : this(BigDecimal(value))
+
+    actual operator fun plus(other: DollarAmount): DollarAmount {
+        return DollarAmount(rawValue.add(other.rawValue))
+    }
+
+    actual operator fun times(other: Int): DollarAmount {
+        return DollarAmount(rawValue.multiply(BigDecimal(other)))
+    }
+}
