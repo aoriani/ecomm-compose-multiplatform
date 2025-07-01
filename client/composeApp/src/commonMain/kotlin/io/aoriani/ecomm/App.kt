43cf0b170fbe9eb0a16ccd1f2d60d3dfe7ce4a00
André Oriani <aoriani@gmail.com>
2025-06-30 23:12:23 -0700

    Integrate Koin for dependency injection
    - Upgraded Koin to version 4.1.0.
    - Added `koin-compose` dependency.
    - Wrapped the `Navigation` composable with `KoinMultiplatformApplication` in `App.kt`.
    - Initialized Koin with `appModule`.
    - Updated Kermit dependency to use `kermit-main`.

--- client/composeApp/src/commonMain/kotlin/io/aoriani/ecomm/App.kt
+++ client/composeApp/src/commonMain/kotlin/io/aoriani/ecomm/App.kt
@@ -3,15 +3,26 @@ package io.aoriani.ecomm
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import io.aoriani.ecomm.di.appModule
 import io.aoriani.ecomm.ui.navigation.Navigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinMultiplatformApplication
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.koinConfiguration
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
 fun App(
     onNavHostReady: suspend (NavController) -> Unit = {}
 ) {
     MaterialTheme {
-        Navigation(onNavHostReady)
+        KoinMultiplatformApplication(
+            config = koinConfiguration {
+                modules(appModule)
+            }
+        ) {
+            Navigation(onNavHostReady)
+        }
     }
 }
