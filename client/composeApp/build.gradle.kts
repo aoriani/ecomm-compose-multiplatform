235128a607334572908b72fa585b6d2e46219d50
André Oriani <aoriani@gmail.com>
2025-07-01 01:08:49 -0700

    Refactor dependency definitions
    - Standardized dependency definitions in `libs.versions.toml` by organizing them alphabetically and by category.
    - Updated `composeApp/build.gradle.kts` to reflect the reordered dependencies from `libs.versions.toml`.

--- client/composeApp/build.gradle.kts
+++ client/composeApp/build.gradle.kts
@@ -5,12 +5,12 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.apolloKotlin)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlinMultiplatform)
 }
 
 kotlin {
@@ -68,39 +68,39 @@ kotlin {
             implementation(libs.kotlinx.coroutines.android)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
+//            implementation(libs.kermit.koin)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.materialIconsExtended)
+            implementation(compose.runtime)
             implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodel.compose)
+            implementation(libs.androidx.navigation.compose)
+            implementation(libs.apollo.adapters.core)
             implementation(libs.apollo.kotlin)
             implementation(libs.apollo.ktor)
-            implementation(libs.apollo.adapters.core)
-            implementation(libs.androidx.navigation.compose)
-            implementation(libs.kotlinx.collections.immutable)
-            implementation(libs.kotlinx.serialization.json)
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor3)
             implementation(libs.kermit.main)
-//            implementation(libs.kermit.koin)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.core)
+            implementation(libs.kotlinx.collections.immutable)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.json)
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.logging)
-            implementation(libs.kotlinx.coroutines.core)
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
             implementation(libs.ktor.client.java)
+            implementation(libs.kotlinx.coroutines.swing)
         }
         val desktopTest by getting {
             dependencies {
