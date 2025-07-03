5acc69d92710775c9a880283272588bd8492d6a3
André Oriani <aoriani@gmail.com>
2025-07-02 20:00:16 -0700

    test: Configure environment for ApplicationTest to include database settings

--- server/src/test/kotlin/dev/aoriani/ecomm/ApplicationTest.kt
+++ server/src/test/kotlin/dev/aoriani/ecomm/ApplicationTest.kt
@@ -2,6 +2,7 @@ package dev.aoriani.ecomm
 
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.config.MapApplicationConfig
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,6 +11,12 @@ class ApplicationTest {
 
     @Test
     fun testRoot() = testApplication {
+        environment {
+            config = MapApplicationConfig(
+                "ecomm.database.url" to "jdbc:sqlite:./data/products.db",
+                "ecomm.database.driver" to "org.sqlite.JDBC"
+            )
+        }
         application {
             module()
         }
