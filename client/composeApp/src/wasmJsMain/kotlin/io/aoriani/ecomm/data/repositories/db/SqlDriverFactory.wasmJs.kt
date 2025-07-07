e4085f462daf71439e2719cac328ca1517efe02c
André Oriani <aoriani@gmail.com>
2025-07-07 20:22:09 -0300

    Testing sqlite for wasm

--- client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
+++ client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
@@ -2,16 +2,22 @@ package io.aoriani.ecomm.data.repositories.db
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import app.cash.sqldelight.driver.worker.createDefaultWebWorkerDriver
+import co.touchlab.kermit.Logger
 import org.w3c.dom.Worker
 
-private val sqlJsWorkerUrl: String = js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
+//private val sqlJsWorkerUrl: String
+//    get() = js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
 
 actual class SqlDriverFactory {
     actual fun createDriver(): SqlDriver {
-        return WebWorkerDriver(
-            Worker(sqlJsWorkerUrl)
-        ).also {
-            ProductDatabase.Schema.create(it)
-        }
+        return createDefaultWebWorkerDriver()
+
+//        Logger.i("ORIANI") { "createDriver -> <$sqlJsWorkerUrl>" }
+//        return WebWorkerDriver(
+//            Worker(sqlJsWorkerUrl)
+//        ).also {
+//            ProductDatabase.Schema.create(it)
+//        }
     }
 }
