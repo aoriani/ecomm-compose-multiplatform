0d8f8277fb078e85ca684139bf6b33e545b5ea72
André Oriani <aoriani@gmail.com>
2025-07-07 21:58:03 -0300

    Cleanup after wasm testings

--- client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
+++ client/composeApp/src/wasmJsMain/kotlin/io/aoriani/ecomm/data/repositories/db/SqlDriverFactory.wasmJs.kt
@@ -1,23 +1,16 @@
 package io.aoriani.ecomm.data.repositories.db
 
+import app.cash.sqldelight.async.coroutines.awaitCreate
 import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import app.cash.sqldelight.driver.worker.createDefaultWebWorkerDriver
-import co.touchlab.kermit.Logger
-import org.w3c.dom.Worker
-
-//private val sqlJsWorkerUrl: String
-//    get() = js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
 
 actual class SqlDriverFactory {
+    //TODO : default web work driver is memory-only
+    // investigate a IndexDB or OPFS implementation
     actual fun createDriver(): SqlDriver {
-        return createDefaultWebWorkerDriver()
-
-//        Logger.i("ORIANI") { "createDriver -> <$sqlJsWorkerUrl>" }
-//        return WebWorkerDriver(
-//            Worker(sqlJsWorkerUrl)
-//        ).also {
-//            ProductDatabase.Schema.create(it)
-//        }
+        val sqlDriver = createDefaultWebWorkerDriver()
+        // TODO - Wasm is assync so this need to be in a coroutine or something
+        //ProductDatabase.Schema.awaitCreate(sqlDriver)
+        return sqlDriver
     }
 }
