5600e239bad60a9a91bbc61c08ab605db0381290
André Oriani <aoriani@gmail.com>
2025-08-06 23:10:08 -0700

    feat: Add "get_product" tool to ProductMcpTools
    - Defined and installed the "get_product" tool using ProductRepository to retrieve product details by ID.
    - Enhanced input schema with validation for a non-blank "id" argument.
    - Added error handling for missing or invalid product IDs.

--- server/src/main/kotlin/dev/aoriani/ecomm/mcp/ProductTools.kt
+++ server/src/main/kotlin/dev/aoriani/ecomm/mcp/ProductTools.kt
@@ -6,7 +6,11 @@ import io.modelcontextprotocol.kotlin.sdk.CallToolResult
 import io.modelcontextprotocol.kotlin.sdk.TextContent
 import io.modelcontextprotocol.kotlin.sdk.Tool
 import io.modelcontextprotocol.kotlin.sdk.server.Server
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.putJsonObject
 
 class ProductMcpTools(private val productRepository: ProductRepository) {
 
@@ -20,14 +24,42 @@ class ProductMcpTools(private val productRepository: ProductRepository) {
         annotations = null,
     )
 
+    private val getProductToolDef = Tool(
+        name = "get_product",
+        description = """
+                    huhuhu
+                """.trimIndent(),
+        inputSchema = Tool.Input(properties = buildJsonObject {
+            putJsonObject("id") {
+                put("type", JsonPrimitive("string"))
+                put("description", JsonPrimitive("Two-letter US state code (e.g. CA, NY)"))
+            }
+        }, required = listOf("id")),
+        outputSchema = null,
+        annotations = null,
+    )
+
 
     fun installTools(mcpServer: Server) {
         mcpServer.addTool(getProductsToolDef, ::getProducts)
+        mcpServer.addTool(getProductToolDef, ::getProduct)
     }
 
     private suspend fun getProducts(request: CallToolRequest): CallToolResult {
         val products = productRepository.getAll()
         val content = products.map { TextContent(it.toString()) }
         return CallToolResult(content = content)
+    }
+
+    private suspend fun getProduct(request: CallToolRequest): CallToolResult {
+        val idArgument = request.arguments["id"]?.jsonPrimitive?.contentOrNull?.takeIf { it.isNotBlank() }
+            ?: return CallToolResult(content = listOf(TextContent("id is required")), isError = true)
+        val product = productRepository.getById(idArgument)
+        return if (product == null) {
+            CallToolResult(content = listOf(TextContent(product.toString())))
+        } else {
+            CallToolResult(content = listOf(TextContent("No product for id:=$idArgument")), isError = true)
+        }
+
     }
 }
